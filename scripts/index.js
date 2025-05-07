1cadb666ed19c6ed7b29b5c8095112351f171875
Ekaterina Degtyariova <ekaterinadegtyariova@yandex.ru>
2025-05-07 12:22:44 +0500

    первый вариант пятого спринта

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const initialCards = window.initialCards;
+// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
+
+// @todo: Функция создания карточки
+function createCard(cardData, deleteCardCallback) {
+    const cardElement = cardTemplate.cloneNode(true); // клонируем шаблон карточки
+    const cardImage = cardElement.querySelector('.card__image'); // собираем элемент из картинки, названия и кнопки "удалить"
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+  
+    deleteButton.addEventListener('click', () => {
+        deleteCardCallback(deleteButton.closest('.card'));
+    });
+  
+    return cardElement;
+  }
+
+  // @todo: Функция удаления карточки
+  function deleteCard(cardElement) {
+    cardElement.remove();
+  }
+
+  initialCards.forEach(cardData => {
+    const card = createCard(cardData, deleteCard);
+    placesList.append(card);
+  });
