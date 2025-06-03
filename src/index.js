8b250782ac1e10369106bf202893dd02eba459d4
Ekaterina Degtyariova <ekaterinadegtyariova@yandex.ru>
2025-06-03 15:54:21 +0500

    первый  вариант шестого спринта

--- src/index.js
+++ src/index.js
@@ -1,19 +1,72 @@
-const path = require("path");
+import './pages/index.css';
+// @todo: Вывести карточки на страницу
+const iconImage = new URL('./images/add-icon.svg', import.meta.url);
+const avatarImage = new URL('./images/avatar.jpg', import.meta.url);
+const card1Image = new URL('./images/card_1.jpg', import.meta.url);
+const card2Image = new URL('./images/card_2.jpg', import.meta.url);
+const card3Image = new URL('./images/card_3.jpg', import.meta.url);
+const closeImage = new URL('./images/close.svg', import.meta.url);
+const deleteImage = new URL('./images/delete-icon.svg', import.meta.url);
+const editImage = new URL('./images/edit-icon.svg', import.meta.url);
+const likeactiveImage = new URL('./images/like-active.svg', import.meta.url);
+const likeinactiveImage = new URL('./images/like-inactive.svg', import.meta.url);
+const logoImage = new URL('./images/logo.svg', import.meta.url);
+
+const initialCards = [
+  // меняем исходные пути на переменные
+  { name: 'Иконка добавления', link: iconImage },
+  { name: 'Аватарка', link: avatarImage },
+  { name: 'Карточка 1', link: card1Image },
+  { name: 'Карточка 2', link: card2Image },
+  { name: 'Карточка 3', link: card3Image },
+  { name: 'Иконка закрытия', link: closeImage },
+  { name: 'Иконка удаления', link: deleteImage },
+  { name: 'Иконка редактирования', link: editImage },
+  { name: 'Лайк', link: likeactiveImage },
+  { name: 'Лайк не активный', link: likeinactiveImage },
+  { name: 'Лого', link: logoImage }
+];
 
 
-module.exports = {
-    entry: './src/scripts/index.js',
-    output: {
-        path: path.resolve(dirname, 'dist'),
-        filename: 'main.js'
-    },
-    mode: 'development',
-    devtool: 'source-map',
-    module: {
-        rules: {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        },
-    }
-};
+  // postcss.config.js
+   const autoprefixer = require('autoprefixer');
+   const cssnano = require('cssnano');
+
+   module.exports = {
+     plugins: [
+       autoprefixer,
+       cssnano({ preset: 'default' })
+     ]
+    };
+// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
+
+// @todo: Функция создания карточки
+function createCard(cardData, deleteCardCallback) {
+   const cardElement = cardTemplate.cloneNode(true); // клонируем шаблон карточки
+   const cardImage = cardElement.querySelector('.card__image'); // собираем элемент из картинки, названия и кнопки "удалить"
+   const cardTitle = cardElement.querySelector('.card__title');
+   const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+   cardImage.src = cardData.link;
+   cardImage.alt = cardData.name;
+   cardTitle.textContent = cardData.name;
+  
+   deleteButton.addEventListener('click', () => {
+       deleteCardCallback(deleteButton.closest('.card'));
+   });
+  
+   return cardElement;
+}
+
+ // @todo: Функция удаления карточки
+ function deleteCard(cardElement) {
+   cardElement.remove();
+ }
+
+ initialCards.forEach(cardData => {
+   const card = createCard(cardData, deleteCard);
+   placesList.append(card);
+ });
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,67 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
+
   output: {
     filename: 'bundle.js',
+        publicPath: './',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, 
+    assetModuleFilename: 'assets/[name].[hash][ext]',
   },
+
   module: {
     rules: [
       {
         test: /\.js$/,
         use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
         use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
-            },
+          MiniCssExtractPlugin.loader, {                 
+            loader: 'css-loader',
+            options: { importLoaders: 1 },
           },
-        ],
+          'postcss-loader'
+        ]
       },
-    ],
+    ] 
   },
+  
+  resolve: {
+    fallback: {
+      "path": false, // или require.resolve("path-browserify")
+      "fs": false,   // В браузере нет доступа к файловой системе
+      "os": false,   // или require.resolve("os-browserify/browser")
+      "url": false   // или require.resolve("url")
+    }
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
   ],
+
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+
+  devtool: 'source-map',
 };
